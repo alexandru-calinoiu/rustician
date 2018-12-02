2b1d4a6d90d4958b1276db1b682ed5178bc67dff
Calinoiu Alexandru Nicolae <calinoiu.alexandru@agilefreaks.com>
2018-12-02 09:40:38 +0200

    Update guess game with error handling

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,6 +4,26 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+
+        Guess {
+            value: value
+        }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
@@ -19,7 +39,7 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to get input");
 
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
                 println!("Invalid number please try again.");
@@ -27,9 +47,11 @@ fn main() {
             }
         };
 
-        println!("Your guess is {}", guess);
+        let guess = Guess::new(guess);
 
-        match guess.cmp(&secret_number) {
+        println!("Your guess is {}", guess.value());
+
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("To small!"),
             Ordering::Greater => println!("To big!"),
             Ordering::Equal => {
